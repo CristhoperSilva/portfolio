e38a43040256e5d99d98ef8d4748470fae2db45b
Cristhoper Silva <c_gomes@outlook.com.br>
2022-11-30 12:55:42 -0300

    Finalizar section home

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
-function menuShow(){
+function menuShow() {
     let menuMobile = document.querySelector('.menu-mobile');
     let menuIcone = document.querySelector('.menu-icone');
-    if (menuMobile.classList.contains('closed')){
+    if (menuMobile.classList.contains('closed')) {
         menuMobile.classList.remove('closed');
         menuIcone.classList.remove('bi-list');
         menuIcone.classList.add('bi-x');
@@ -10,5 +10,19 @@ function menuShow(){
         menuMobile.classList.add('closed');
         menuIcone.classList.remove('bi-x');
         menuIcone.classList.add('bi-list');
+    }
+}
+
+var i = 0;
+
+function typeWriter() {    
+    let txt = 'Desenvolvedor Front-End. '
+    let speed = 100;
+    document.getElementById("stack-html").classList.add('closed')
+    if (i < txt.length) {
+        document.getElementById("stack").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+
     }
 }
